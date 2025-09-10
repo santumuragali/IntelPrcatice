bdec6b5ea991a0ad2686d24c710927e2759e0e0a
santumuragali <annappamuragali@gmail.com>
2025-09-10 22:58:51 +0530

    first changes from intel j

---
+++ src/main/java/code/practice/FibonicSeries.java
@@ -0,0 +1,17 @@
+package code.practice;
+
+public class FibonicSeries {
+
+    public static void main(String[] args) {
+       int first = 0;
+       int second  =1;
+        System.out.print(first +" "+second +" ");
+       for(int i = 0;i<6;i++){
+           int result = first+second;
+           first = second;
+           second =result;
+           System.out.print(result +" ");
+
+       }
+    }
+}
