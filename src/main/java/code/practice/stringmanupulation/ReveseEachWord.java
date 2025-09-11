d5aed7f3db1671bd3f8554c889a4544ddc2ef964
santumuragali <annappamuragali@gmail.com>
2025-09-11 09:38:10 +0530

    reverse string of each word

---
+++ src/main/java/code/practice/stringmanupulation/ReveseEachWord.java
@@ -0,0 +1,25 @@
+package code.practice.stringmanupulation;
+
+public class ReveseEachWord {
+
+    public static void main(String[] args) {
+        ReveseEachWord re = new ReveseEachWord();
+        re.reverseEachWord("Java is programing lanaguges");
+    }
+    public  void reverseEachWord(String input){
+        String[] str = input.split(" ");
+        StringBuilder res = new StringBuilder();
+//        StringBuilder val = new StringBuilder();
+        for(String out : str){
+            String rev = "";
+//            val.delete(0,val.length());
+            for(char ch : out.toCharArray()){
+               rev = ch + rev;
+//               val.insert(0,ch);
+            }
+            System.out.println(val);
+            res.append(rev).append(" ");
+        }
+        System.out.println(res);
+    }
+}
