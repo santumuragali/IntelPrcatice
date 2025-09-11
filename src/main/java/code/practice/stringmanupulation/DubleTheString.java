79cfc2806ee88a53bc4d4777e8e1d5ebab0254c3
santumuragali <annappamuragali@gmail.com>
2025-09-11 23:16:58 +0530

    Swap of number

--- src/main/java/code/practice/stringmanupulation/DubleTheString.java
+++ src/main/java/code/practice/stringmanupulation/DubleTheString.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class DubleTheString {
 
     public static void main(String[] args) {
-        Scanner sc  = new Scanner(System.in);
-        String inp = sc.nextLine();
-        System.out.println("enter the input of string");
-        dobleChar(inp);
-        sc.close();
+//        Scanner sc  = new Scanner(System.in);
+//        System.out.println("enter the input of string");
+//        String inp = sc.nextLine();
+        dobleChar("ram");
+//        sc.close();
     }
     public static void dobleChar(String str){
         StringBuilder res = new StringBuilder();
